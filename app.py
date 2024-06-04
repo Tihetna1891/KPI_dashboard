50d3ca899678e8949d1cb76fec91c4d864c906a0
Tihetna Mesfin <tihetna1mm@gmail.com>
2024-06-04 13:07:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,204 +1,17 @@
-# import requests
-
-# from map import *
-# # Sidebar inputs
-# start_date, end_date, time_frame = sidebar()
-
-# # Load data from the database
-# orders = get_orders(start_date, end_date)
-# products = get_product()
-# product_names = get_product_names()
-
-
-# products = products.merge(product_names, on='name_id')
-
-
-# total_sales = calculate_total_sales(orders, products, time_frame)
-# order_volume = calculate_order_volume(orders, products, time_frame)
-# average_order_value = calculate_average_order_value(orders, products, time_frame)
-# fulfillment_time = calculate_fulfillment_time(orders, products, time_frame)
-# product_popularity_data = product_popularity(orders, products, time_frame)
-# # order_volume_by_status = calculate_order_volume_by_status(orders, products, time_frame)
-
-# # Summarize the most sold products
-# most_sold_products = total_sales.groupby('product_name')['total_sales'].sum().reset_index().sort_values(by='total_sales', ascending=False)
-# sorted_product_names = most_sold_products['product_name'].tolist()
-
-# # Get unique vendor names for selection
-# unique_vendor_names = products['vendor_name'].unique()
-# selected_vendors = st.sidebar.multiselect("Select vendors", unique_vendor_names, default=unique_vendor_names)
-
-# # Display products sorted by most sold
-# selected_products = st.sidebar.multiselect("Select the most sold products", sorted_product_names, default=sorted_product_names)
-
-
-# # Filter data based on selected vendors
-# total_sales = total_sales[total_sales['vendor_name'].isin(selected_vendors) & (total_sales['product_name'].isin(selected_products))]
-# # st.write("Filtered Total Sales Data", total_sales.head())
-# order_volume = order_volume[order_volume['vendor_name'].isin(selected_vendors) & (order_volume['product_name'].isin(selected_products))]
-# average_order_value = average_order_value[average_order_value['vendor_name'].isin(selected_vendors) & (average_order_value['product_name'].isin(selected_products))]
-# fulfillment_time = fulfillment_time[fulfillment_time['vendor_name'].isin(selected_vendors) & (fulfillment_time['product_name'].isin(selected_products))]
-# product_popularity_data = product_popularity_data[product_popularity_data['vendor_name'].isin(selected_vendors) & (product_popularity_data['product_name'].isin(selected_products))]
-
-# # Streamlit dashboard
-# st.title('Vendor Performance KPI')
-# # Summary section
-# st.subheader("Summary")
-# st.markdown(f"**Total Sales:** {total_sales['total_sales'].sum():,.2f}")
-# st.markdown(f"**Total Orders:** {order_volume['order_count'].sum()}")
-# st.markdown(f"**Average Order Value:** {average_order_value['average_order_value'].mean():,.2f}")
-
-# top_products = most_sold_products.head(5)
-# st.markdown("**Top 5 Most Sold Products:**")
-# for i, row in top_products.iterrows():
-#     st.markdown(f"- {row['product_name']}: {row['total_sales']:,.2f}")
-
-
-# total_sales['vendor_product'] = total_sales['vendor_name'] + ' - ' + total_sales['product_name']
-# st.header('Total sales by Vendor')
-# # st.write("Total Sales Data for Chart", total_sales)
-# st.markdown("""
-# ### Total Sales
-# This metric calculates the total revenue generated from sales over a specified period. It is calculated by summing the sales revenue for each product sold.
-# """)
-# sales_chart = alt.Chart(total_sales).mark_line().encode(
-#     x='date:T',
-#     y='total_sales:Q',
-#     color='vendor_product:N',
-#     tooltip=['vendor_name','product_name', 'date', 'total_sales']
-# ).interactive().properties(title='Total Sales Over Time')
-
-# st.altair_chart(sales_chart, use_container_width=True)
-# # Loop through each selected vendor and create separate charts
-# for vendor in selected_vendors:
-#     st.subheader(f'Total Sales for {vendor}')
-#     vendor_data = total_sales[total_sales['vendor_name'] == vendor]
-    
-#     if not vendor_data.empty:
-#         vendor_data['vendor_product'] = vendor_data['product_name']
-        
-#         sales_chart = alt.Chart(vendor_data).mark_line().encode(
-#             x='date:T',
-#             y='total_sales:Q',
-#             color='vendor_product:N',
-#             tooltip=['vendor_name', 'product_name', 'date', 'total_sales']
-#         ).interactive().properties(
-#             title=f'Total Sales Over Time for {vendor}'
-#         )
-        
-#         st.altair_chart(sales_chart, use_container_width=True)
-#     else:
-#         st.write(f"No data available for {vendor}")
-        
-# # # Order Volume by Vendor
-# order_volume['vendor_product'] = order_volume['vendor_name'] + ' - ' + order_volume['product_name']
-# st.header('Order Volume by Vendor')
-# st.markdown("""
-# ### Order Volume
-# This metric measures the total number of COMPLETED orders placed over a specified period. It indicates the quantity of products sold.
-# """)
-# volume_chart = alt.Chart(order_volume).mark_line().encode(
-#     x=alt.X('date:T', title='Date' if time_frame == 'Daily' else 'Time Frame'),
-#     y='order_count:Q',
-#     color='vendor_name:N',
-#     tooltip=['vendor_name','product_name', 'date', 'order_count']
-# ).interactive().properties(title='Order Volume Over Time')
-# st.altair_chart(volume_chart, use_container_width=True)
-
-# for vendor in selected_vendors:
-#     st.subheader(f'Order Volume for {vendor}')
-#     vendor_data = order_volume[order_volume['vendor_name'] == vendor]
-    
-#     if not vendor_data.empty:
-#         vendor_data['vendor_product'] = vendor_data['product_name']
-        
-#         sales_chart = alt.Chart(vendor_data).mark_line().encode(
-#             x='date:T',
-#             y='order_count:Q',
-#             color='vendor_product:N',
-#             tooltip=['vendor_name', 'product_name', 'date', 'order_count']
-#         ).interactive().properties(
-#             title=f'Total Sales Over Time for {vendor}'
-#         )
-        
-#         st.altair_chart(sales_chart, use_container_width=True)
-#     else:
-#         st.write(f"No data available for {vendor}")
-
-
-# # # Average Order Value by Vendor
-# st.header('Average Order Value by Vendor')
-# st.markdown("""
-# ### Average Order Value
-# This metric calculates the average revenue per order. It is calculated by dividing the total sales revenue by the number of orders.
-# """)
-# aov_chart = alt.Chart(average_order_value).mark_line().encode(
-#     x='date:T',
-#     y='average_order_value:Q',
-#     color='vendor_name:N',
-#     tooltip=['vendor_name','product_name', 'date', 'average_order_value']
-# ).interactive().properties(title='Average Order Value Over Time')
-# st.altair_chart(aov_chart, use_container_width=True)
-
-
-# st.header('Fulfillment Time by Vendor')
-# st.markdown("""
-# ### Fulfillment Time
-# This metric measures the average time taken to fulfill orders from the time they are placed to the time they are delivered. It indicates the efficiency of the fulfillment process.
-# """)
-# fulfillment_time_chart = alt.Chart(fulfillment_time).mark_line().encode(
-#     x=alt.X('time_frame:T', title='Date' if time_frame == 'Daily' else 'Time Frame'),
-#     y='fulfillment_time:Q',
-#     color='vendor_name:N',
-#     tooltip=['vendor_name','product_name', 'fulfillment_time']
-# ).interactive().properties(title='Fulfillment Time by Vendor')
-# st.altair_chart(fulfillment_time_chart, use_container_width=True)
-
-# product_popularity_data['vendor_product'] = product_popularity_data['vendor_name'] + ' - ' + product_popularity_data['product_name']
-# st.header('Product Popularity by Vendor')
-# st.markdown("""
-# ### Product Popularity
-# This metric measures the popularity of each product based on the number of units sold. It helps identify the best-selling products.
-# """)
-# product_popularity_data_chart = alt.Chart(product_popularity_data).mark_line().encode(
-#     x=alt.X('time_frame:T', title='Date' if time_frame == 'Daily' else 'Time Frame'),
-#     y='product_popularity:Q',
-#     color='vendor_name:N',
-#     # tooltip=['vendor_name','product_name','product_popularity']
-# ).interactive().properties(title='Product Popularity by Vendor')
-# st.altair_chart(product_popularity_data_chart, use_container_width=True)
-
-# for vendor in selected_vendors:
-#     st.subheader(f'Product Popularity by {vendor}')
-#     vendor_data = product_popularity_data[product_popularity_data['vendor_name'] == vendor]
-    
-#     if not vendor_data.empty:
-#         vendor_data['vendor_product'] = vendor_data['product_name']
-        
-#         sales_chart = alt.Chart(vendor_data).mark_line().encode(
-#             x=alt.X('time_frame:T', title='Date' if time_frame == 'Daily' else 'Time Frame'),
-#             y='product_popularity:Q',
-#             color='vendor_product:N',
-#             tooltip=['vendor_name', 'product_name', 'product_popularity']
-#         ).interactive().properties(
-#             title=f'Product Popularity by {vendor}'
-#         )
-        
-#         st.altair_chart(sales_chart, use_container_width=True)
-#     else:
-#         st.write(f"No data available for {vendor}")
-
-
-
-import requests
+import datetime
 import pandas as pd
 import streamlit as st
 import altair as alt
 from map import *
 
-# Sidebar inputs
-start_date, end_date, time_frame = sidebar()
 
+# Sidebar inputs
+start_date = st.sidebar.date_input("Start date", datetime.datetime(2023, 1, 1))
+end_date = st.sidebar.date_input("End date", datetime.datetime.today())
+time_frame = st.sidebar.selectbox("Select time frame", ["Daily", "Weekly", "Monthly", "Yearly"])
+# start_date, end_date, time_frame = sidebar()
+
+# Load data from the database
 # Load data from the database
 orders = get_orders(start_date, end_date)
 products = get_product()
@@ -207,6 +20,7 @@ product_names = get_product_names()
 products = products.merge(product_names, on='name_id')
 
 total_sales = calculate_total_sales(orders, products, time_frame)
+total_sales['date'] = pd.to_datetime(total_sales['date'])
 order_volume = calculate_order_volume(orders, products, time_frame)
 average_order_value = calculate_average_order_value(orders, products, time_frame)
 fulfillment_time = calculate_fulfillment_time(orders, products, time_frame)
@@ -227,16 +41,19 @@ unique_vendor_names = products['vendor_name'].unique()
 selected_vendors = st.sidebar.multiselect("Select vendors", unique_vendor_names, default=unique_vendor_names)
 
 # Display products sorted by most sold
-selected_products = st.sidebar.multiselect("Select the most sold products", sorted_product_names, default=sorted_product_names)
+selected_products = st.sidebar.multiselect("Select products", sorted_product_names, default=sorted_product_names)
 
 # Filter data based on selected vendors
+
 total_sales = total_sales[total_sales['vendor_name'].isin(selected_vendors) & (total_sales['product_name'].isin(selected_products))]
+total_sales['total_sales'] = pd.to_numeric(total_sales['total_sales'], errors='coerce')
 order_volume = order_volume[order_volume['vendor_name'].isin(selected_vendors) & (order_volume['product_name'].isin(selected_products))]
 average_order_value = average_order_value[average_order_value['vendor_name'].isin(selected_vendors) & (average_order_value['product_name'].isin(selected_products))]
 fulfillment_time = fulfillment_time[fulfillment_time['vendor_name'].isin(selected_vendors) & (fulfillment_time['product_name'].isin(selected_products))]
 product_popularity_data = product_popularity_data[product_popularity_data['vendor_name'].isin(selected_vendors) & (product_popularity_data['product_name'].isin(selected_products))]
 
 total_sales_vendor = total_sales_vendor[total_sales_vendor['vendor_name'].isin(selected_vendors) ]
+total_sales_vendor['total_sales'] = pd.to_numeric(total_sales_vendor['total_sales'], errors='coerce')
 order_volume_vendor = order_volume_vendor[order_volume_vendor['vendor_name'].isin(selected_vendors)]
 average_order_value_vendor= average_order_value_vendor[average_order_value_vendor['vendor_name'].isin(selected_vendors)]
 fulfillment_time_vendor = fulfillment_time_vendor[fulfillment_time_vendor['vendor_name'].isin(selected_vendors)]
@@ -272,10 +89,19 @@ custom_colors = [
 
 st.header('Total Sales by Vendor')
 # Metrics Descriptions
+
+# total_sales['vendor_product'] = total_sales['vendor_name'] + ' - ' + total_sales['product_name']
+# st.write("Filtered Total Sales Data for Chart", total_sales)
+st.header('Total sales by Vendor')
 st.markdown("""
 ### Total Sales
 This metric calculates the total revenue generated from sales over a specified period. It is calculated by summing the sales revenue for each product sold.
 """)
+
+# Check the filtered data
+# st.write("Filtered Total Sales Data", filtered_sales)
+# st.write("Data Types of Filtered Sales Data", filtered_sales.dtypes)
+# Create the chart
 sales_chart = alt.Chart(total_sales_vendor).mark_line().encode(
     x='date:T',
     y='total_sales:Q',
